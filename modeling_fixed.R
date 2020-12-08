36bb820e7393a39dce69de56e47b6158ab7313ae
Sofi <sofi.sinozich@gmail.com>
2020-12-08 18:05:00 -0500

    Add interactions

--- modeling_fixed.R
+++ modeling_fixed.R
@@ -1,6 +1,7 @@
 full_data <- readRDS("data/full_data.rds")
 
 full_data$race <- factor(full_data$race,levels=c("White","Black","Hispanic","Other"))
+full_data$black <- ifelse(full_data$race=="Black",1,0)
 
 full_data$examiner_id_factor <- relevel(factor(full_data$examiner_id,levels=as.character(c(3007,c(3001:3012)[-7]))),ref="3007")
 
@@ -30,6 +31,19 @@ health_lasso_weighted <- cv.glmnet(health_status2 ~
                                   weights = WTPFHX6,
                                   data=full_data,
                                   family= binomial(link = "logit"))
+
+health_lasso_weightaspredictor <- cv.glmnet(health_status2 ~
+                                     scale(WTPFHX6) +
+                                     race+ gender + age + region + time_of_day +
+                                     scale(systolic_bp) + scale(diastolic_bp) +
+                                     locomotion + paralysis + impairment + infection +
+                                     scale(bmi) + scale(weight) + scale(height)+ 
+                                     scale(rbc_count) + scale(wbc_count) + scale(plt_count) + scale(hemoglobin) +
+                                     examiner_id_factor,
+                                   data=full_data,
+                                   family= binomial(link = "logit"))
+
+
 
 coef(health_lasso,s="lambda.1se")
 coef(health_lasso_weighted,s="lambda.1se")
@@ -69,15 +83,24 @@ health_model_examiner <- glm(health_status2 ~
                     data=full_data,
                     family = binomial(link = "logit"))
 
+health_model_examiner_gender <- update(health_model_examiner,.~.+examiner_id_factor:gender)
+health_model_examiner_black <- update(health_model_examiner,.~.+examiner_id_factor:black)
+health_model_examiner_blackgender <- update(health_model_examiner,.~.+examiner_id_factor:gender:black)
+
 AIC(health_model,health_model_lim,health_model_examiner)
 # Version with only limited to avoid the warning
 AIC(health_model_lim,health_model_examiner)
+
+AIC(health_model_examiner,health_model_examiner_gender,health_model_examiner_black,health_model_examiner_blackgender)
 
 # LRT
 (health_model_lim$deviance - health_model_examiner$deviance)>qchisq(.95,11)
 # This is greater, so we conclude that we should keep the examiner IDs
 
-summary(health_model_examiner)
+(health_model_examiner$deviance - health_model_examiner_gender$deviance)>qchisq(.95,11)
+# This is greater, so we conclude that we should keep the examiner IDs
+
+summary(health_model_examiner_gender)
 
 
 # Diagnostics -------------------------------------------------------------
@@ -114,7 +137,3 @@ with(subset(outcomes,health_status2 == 1),{
 # A significant improvement!
 
 # Try ROC
-
-# Machine learning methods?? ------------------------------------------------
-
-
